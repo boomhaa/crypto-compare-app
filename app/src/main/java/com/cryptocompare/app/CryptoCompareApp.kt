87e0c778799574ba97089824aae0cfffba14ebf1
boomhaa~ <90379111+boomhaa@users.noreply.github.com>
2026-04-04 00:25:19 +0300

    fix for build release

--- app/src/main/java/com/cryptocompare/app/CryptoCompareApp.kt
+++ app/src/main/java/com/cryptocompare/app/CryptoCompareApp.kt
@@ -5,11 +5,13 @@ import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import com.cryptocompare.app.worker.WorkScheduler
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class CryptoCompareApp :
     Application(),
     Configuration.Provider {
+    @Inject
     lateinit var hiltWorkerFactory: HiltWorkerFactory
 
     override val workManagerConfiguration: Configuration
