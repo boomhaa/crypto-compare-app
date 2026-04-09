c759b3bf6866a180b0d8b0c4e3a52822bfb1299c
boomhaa~ <90379111+boomhaa@users.noreply.github.com>
2026-04-09 17:50:19 +0300

    [#27] feature: added use cases

---
+++ core/domain/src/main/java/com/cryptocompare/domain/usecase/pairs/ToggleFavouriteTickerUseCase.kt
@@ -0,0 +1,12 @@
+package com.cryptocompare.domain.usecase.pairs
+
+import com.cryptocompare.domain.repository.FavouriteTickerRepository
+import javax.inject.Inject
+
+class ToggleFavouriteTickerUseCase @Inject constructor(
+    private val favouriteTickerRepository: FavouriteTickerRepository
+) {
+    suspend operator fun invoke(ticker: String): Result<Boolean>{
+        return favouriteTickerRepository.toggleFavouriteTicker(ticker)
+    }
+}
