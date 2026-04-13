48cfd6d0b3ac3831b6de78fa99f05b6dad9f0f13
boomhaa~ <90379111+boomhaa@users.noreply.github.com>
2026-04-13 21:02:02 +0300

    [#27] feature: fix

--- feature/pairs/src/main/java/com/cryptocompare/pairs/ui/screens/mainScreen/MainScreen.kt
+++ feature/pairs/src/main/java/com/cryptocompare/pairs/ui/screens/mainScreen/MainScreen.kt
@@ -17,9 +17,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
