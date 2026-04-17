d216788773690724e68bc38360b3cb67cc5c9679
boomhaa~ <90379111+boomhaa@users.noreply.github.com>
2026-04-17 22:13:05 +0300

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.boomhaa.cryptocompare"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
